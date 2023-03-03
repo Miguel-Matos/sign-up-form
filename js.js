637c0fd8df9a5c22493ee16da96aef791a70189e
Miguel Matos <18700730+Miguel-Matos@users.noreply.github.com>
2023-03-03 22:58:22 +0900

    Completed initial requirements

--- js.js
+++ js.js
@@ -3,3 +3,7 @@ let password = document.querySelector("#password");
 let password2 = document.querySelector("#password2");
 
 let error = document.querySelector(".error");
+
+error.oninvalid = function(event) {
+        event.target.setCustomValidity('Minimum 8 characters, upper and lowercase, number, and symbol');
+    }
